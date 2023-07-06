1e0acb1ac30b8b048e2e630ac11844ec81c79154
Alex Kharlamov <aakharlam@gmail.com>
2023-07-06 20:55:22 +0400

    prepared for render

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 import app from './app.js';
 import connectDB from './middleware/connectDB.js';
 
-// dotenv.config({ path: 'process.env' });
+dotenv.config({ path: 'config.env' });
 
 //
 
-// connect DB and run server (in the connectDB callback)
 connectDB().then(() => {
   const port = process.env.PORT;
   app.listen(port, () => {
